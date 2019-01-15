9ecd80ffbe2917d1d36303598fb9e0e9749411d1
comical-desk-9778 <reginald.beauvais@gmail.com>
2019-01-15 05:49:40 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+   return string.toLowerCase()
+  }
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+     if (string.toLowerCase){
+       return " I love you, too";
+     }
+}
+
+function sayHiToGrandma(string) {
+  return "YES INDEED!".toUpperCase()
+}
+
+function sayHiToGrandma(string) {
+  return "I love you, too"
+}
